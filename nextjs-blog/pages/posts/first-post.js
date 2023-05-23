ea29fca25b949b77db4ba97def194be2adababec
DiviyamPathak <patdiviyam@gmail.com>
2023-05-23 19:53:18 +0530

    continuation

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function Firstpost() {
+    return (<>
+        <h1>Heloo </h1>
+        <h3>
+            this is learnig page
+        </h3>
+        <p>use this to go to <Link href="/">Home Page</Link> </p>
+        </>
+    )
+}

0f2bdfa50cff1ae675f84e4f2a46ee4ac2918941
DiviyamPathak <patdiviyam@gmail.com>
2023-08-13 00:45:22 +0530

    sd

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,28 @@
 import Link from "next/link"
-
+import Head from "next/head"
+import Script from "next/script"
+import Layout from "../../Components/layout"
 export default function Firstpost() {
-    return (<>
+    return (
+        <Layout>
+        <Head>
+            <title>First Post</title>
+
+        </Head>
+        <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
         <h1>Heloo </h1>
         <h3>
             this is learnig page
         </h3>
         <p>use this to go to <Link href="/">Home Page</Link> </p>
-        </>
+        <img src="/images/profilepic.jpeg" alt="Diviyam" />
+
+        </Layout>
     )
 }
